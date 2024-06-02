cb16e65d31309252770bf9538876ff0d67937243
Gallost <harryhe187@gmail.com>
2024-06-02 03:26:37 -0400

    Zoom-scrolling page design

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,7 +1,27 @@
-const navButtons = document.getElementById("nav-buttons");
-const welcomeItems = document.getElementById("welcome-items");
-Array.from(navButtons.children).forEach(child => {
-    child.addEventListener("click", () => {
-        welcomeItems.classList.add("navbar-mode");
-    });
+console.clear();
+
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener("load", () => {
+    gsap
+        .timeline({
+        scrollTrigger: {
+            trigger: ".wrapper",
+            start: "top top",
+            end: "+=80%",
+            pin: true,
+            scrub: true,
+            // markers: true
+        }
+        })
+        .to("#title-splash-wrapper", {
+            scale: 2,
+            z: 350,
+            transformOrigin: "center center",
+            ease: "power1.inOut",
+            opacity: 0
+        }, 0)
+        .to(".content section", {
+            opacity: 1
+        }, 0.2)
 });
