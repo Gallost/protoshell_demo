6a27e5259c8e4f8b494bfca65f9fa1d69d1a1059
Gallost <harryhe187@gmail.com>
2024-06-03 20:07:03 -0400

    Tune the animation to zoom into the 'O' in ProtoShell

--- scripts/animations.js
+++ scripts/animations.js
@@ -3,25 +3,44 @@ console.clear();
 gsap.registerPlugin(ScrollTrigger);
 
 window.addEventListener("load", () => {
+    // Reset the scroll position
+    window.scrollTo(0, 0);
+
+    // Animate based on scroll
     gsap
         .timeline({
         scrollTrigger: {
             trigger: ".wrapper",
             start: "top top",
-            end: "+=80%",
+            end: "+=150%",  // Scroll length is 1.5x the element height
             pin: true,
             scrub: true,
-            // markers: true
+            markers: true
         }
         })
         .to("#title-splash-wrapper", {
-            scale: 2,
+            scale: 130,
             z: 350,
             transformOrigin: "center center",
-            ease: "power1.inOut",
-            opacity: 0
+            ease: "power4.inOut",
+        }, 0) // This is the 'offset' from which the animation will begin playing
+              // Animations with the same 'offset' will begin playing simultaneously
+              // Otherwise, the animations will play one after another
+        .to("#title-splash-wrapper", {
+            opacity: 0,
+            ease: "expo.in"
         }, 0)
-        .to(".content section", {
-            opacity: 1
-        }, 0.2)
+        .to("#circle-cutout", {
+            scale: 130,
+            transformOrigin: "center",
+            ease: "power4.inOut",
+        }, 0)
+        .to("#circle-cutout", {
+            opacity: 0,
+            ease: "expo.in"
+        }, 0)
+        .to("#circle-cutout > div", {
+            backgroundColor: "white",
+            duration: 0.1
+        }, 0)
 });
